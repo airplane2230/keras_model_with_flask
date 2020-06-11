28a46ff93999d41599f3aad9693f9577eda5491c
johwiyong <airplane2230@github.com>
2020-06-11 21:35:47 +0900

    no change

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ from flask import Flask, render_template, redirect, request, url_for
 import numpy as np
 import cv2
 from urllib.parse import unquote
+import urllib
 import base64
 import re
+import urllib.request
 
 app = Flask(__name__)
 
 @app.route('/doPredict', methods = ["POST"])
-def do_predict():
-    data = request.values['img'].split(',')[1]
-    decoded_data = base64.b64decode(data)
-    np_img = np.fromstring(decoded_data, dtype=np.uint8)
+def doPredict():
+    img_url = request.form.get('url')
+    img_url = img_url.split(',')[1]
 
-    print(np_img.shape)
+    decoded_img = base64.b64decode(img_url)
+    with open("./test.png", "wb") as save_file:
+        save_file.write(decoded_img)
+
+    # print(np.fromstring(img, dtype = np.uint8).shape)
+    # with open('./img.png', 'wb') as save_file:
+    #     save_file.write(urllib.request.urlopen(img).read())
+
+    # data = request.values['img'].split(',')[1]
+    # decoded_data = base64.b64decode(data)
+    # np_img = np.fromstring(decoded_data, dtype=np.uint8)
 
     return ""
 
